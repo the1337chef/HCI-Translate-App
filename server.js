040bd1393ce76ed00d457e084e244edabdc7c962
the1337chef <spidertheo@yahoo.com>
2018-02-06 02:03:06 -0500

    changed port

--- server.js
+++ server.js
@@ -82,6 +82,5 @@ app.get('/', function (req, res) {
     Translate ();
   })
 
-app.listen(3000, function () {
-    console.log('listening on port 3000')
+app.listen(process.env.PORT || 5000, function () {
   })
